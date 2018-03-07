cb38d41d3ef6d4ba6267bfd9e0ee58f653a301df
20854510+ <TheBrokenGod@users.noreply.github.com>
2018-03-07 13:50:29 +0100

    Final doc

--- AspectJ-Memento/consegna/src/compile.sh
+++ AspectJ-Memento/consegna/src/compile.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-ajc -1.8 -argfile sources.lst -outjar ../bin/apps.jar
+find -follow -type f -name "*.java" > temp.lst 
+find -follow -type f -name "*.aj" >> temp.lst
+ajc -1.8 -argfile temp.lst -outjar $1
+rm temp.lst
+cp $(find -follow -type f -name "*.jar") $(dirname $1)
+echo "#!/bin/bash" > $(dirname $1)/start.sh
+echo "java -cp \"*\"" $2>> $(dirname $1)/start.sh
+chmod +x $(dirname $1)/start.sh
+echo "java -cp \"*\"" $2 >> $(dirname $1)/start.bat
